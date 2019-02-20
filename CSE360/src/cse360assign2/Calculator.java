51b9a88cdad10d1de8848c6ede341b0148af387a
Green99 <Green99@LAPTOP-6J8DTO18>
2019-02-20 07:49:33 -0700

    Initial Commit of Assign2 CSE360

---
+++ CSE360/src/cse360assign2/Calculator.java
@@ -0,0 +1,76 @@
+/**
+ * Name: Allaina Ispas
+ * ID: 1213729359
+ * Description: This program functions as that of a normal calculator
+ * 
+ * Due: 2/25/19
+ * 
+ * Edits to code noted at the end of class
+ */
+package cse360assign2;
+
+public class Calculator {
+/**
+ * Initializes the int value total
+ */
+	private int total;
+	
+	/**
+	 * Initializes the Calculator method
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * Returns the total as calculated
+	 * @return total
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * A method that adds the value to the total
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * A method that subtracts the value from the total
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * A method that multiples the value and the total
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/**
+	 * A method that divides the total by the value
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/**
+	 * This method returns the history of what was calculated in the method
+	 * @return The string of values used 
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
+
+/**
+ * No Edits made except the javadoc comments currently
+ */
